cc85c058fe0344172bb27ee08f7d5fae06fdc8cb
chris <acomputerdog@gmail.com>
2017-01-14 13:42:50 -0500

    Make /reload safe

--- src/net/acomputerdog/greifcontrol/PluginGreifControl.java
+++ src/net/acomputerdog/greifcontrol/PluginGreifControl.java
@@ -3,6 +3,7 @@ package net.acomputerdog.greifcontrol;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,17 +22,10 @@ public class PluginGreifControl extends JavaPlugin implements Listener {
     public void onEnable() {
         containers = new boolean[Material.values().length];
         materials = new Material[]{Material.CHEST, Material.TRAPPED_CHEST, Material.DISPENSER, Material.FURNACE, Material.DROPPER, Material.ENCHANTMENT_TABLE, Material.ANVIL, Material.HOPPER};
+
         for (Material mat : materials) {
             containers[mat.ordinal()] = true;
         }
-        //Material[] mats = Material.values();
-        /*
-        for (int i = 0; i < mats.length; i ++) {
-            Material m = mats[i];
-            //true if material is a container
-            containers[i] = (m == Material.CHEST || m == Material.TRAPPED_CHEST || m == Material.DISPENSER || m == Material.FURNACE || m == Material.DROPPER || m == Material.ENCHANTMENT_TABLE || m == Material.ANVIL || m == Material.HOPPER);
-        }
-        */
 
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -40,6 +34,8 @@ public class PluginGreifControl extends JavaPlugin implements Listener {
     public void onDisable() {
         containers = null;
         materials = null;
+
+        HandlerList.unregisterAll((JavaPlugin) this);
     }
 
     @EventHandler
@@ -47,9 +43,7 @@ public class PluginGreifControl extends JavaPlugin implements Listener {
         List<Block> blocks = new ArrayList<>(e.blockList().size());
         blocks.addAll(e.blockList());
         e.blockList().clear();
-        //if not a container
-        blocks.stream().filter(b -> !containers[e.getBlock().getType().ordinal()]).forEach(b -> {
-            e.blockList().add(b);
-        });
+        //if not a container, add to list of destroyed blocks
+        blocks.stream().filter(b -> !containers[e.getBlock().getType().ordinal()]).forEach(b -> e.blockList().add(b));
     }
 }
